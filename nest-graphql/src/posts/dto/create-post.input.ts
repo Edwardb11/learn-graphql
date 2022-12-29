257a10907a6ce700294bb1f307c6d805002213af
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-29 16:13:36 -0400

    feat:class validation

--- nest-graphql/src/posts/dto/create-post.input.ts
+++ nest-graphql/src/posts/dto/create-post.input.ts
@@ -1,10 +1,15 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class CreatePostInput {
+  @MinLength(3, { message: 'Titulo muy corto ' })
+  @MaxLength(100, { message: 'Titulo muy largo ' })
+  @IsNotEmpty({ message: 'Titulo es requerido ' })
   @Field()
   title: string;
 
-  @Field()
-  content: string;
+  @MaxLength(400)
+  @Field({ nullable: true })
+  content?: string;
 }
