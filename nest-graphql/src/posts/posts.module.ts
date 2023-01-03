cea721da715be1ff334b3865c1e733e55d2d08e3
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2023-01-03 11:38:04 -0400

    fix

--- nest-graphql/src/posts/posts.module.ts
+++ nest-graphql/src/posts/posts.module.ts
@@ -4,10 +4,9 @@ import { PostsService } from './posts.service';
 import { PostsResolver } from './posts.resolver';
 import { Post } from './post.entity';
 import { AuthorsModule } from 'src/authors/authors.module';
-import { AuthorsService } from 'src/authors/authors.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post]), AuthorsModule, AuthorsService],
+  imports: [TypeOrmModule.forFeature([Post]), AuthorsModule],
   providers: [PostsService, PostsResolver],
 })
 export class PostsModule {}
