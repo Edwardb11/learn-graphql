86cc390744d117ef1c748d9396f93c6707d5815e
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2023-01-04 11:04:14 -0400

    entities and relationship

--- nest-gql-pets/src/owners/entities/owner.entity.ts
+++ nest-gql-pets/src/owners/entities/owner.entity.ts
@@ -1,7 +1,19 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Pet } from '../../pets/entities/pet.entity';
 
+@Entity()
 @ObjectType()
 export class Owner {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @OneToMany(() => Pet, (pet) => pet.owner)
+  @Field((type) => [Pet], { nullable: true })
+  pets?: Pet[];
 }
