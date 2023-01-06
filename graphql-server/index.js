654f7be57adfcac7dc125b9e9a29432c3dd7ecb4
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2023-01-06 13:11:02 -0400

    running server

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,4 +1,4 @@
-const { gql } = require("apollo-server");
+const { gql, ApolloServer } = require("apollo-server");
 
 const persons = [
   {
@@ -53,3 +53,11 @@ const resolvers = {
   },
 };
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server running ${url}`);
+});
