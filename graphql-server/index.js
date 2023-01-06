b434be43a6d3366e81ee38500f2761610233ce15
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2023-01-06 13:05:55 -0400

    Definitions graqhql

---
+++ graphql-server/index.js
@@ -0,0 +1,47 @@
+const { gql } = require("apollo-server");
+
+const persons = [
+  {
+    id: "jhahahh-1bnsbsb-bsbsb0011-11sbs",
+    name: "Edward",
+    street: "La Vega",
+    citi: "La Vega",
+    phone: "294829829",
+  },
+  {
+    id: "kdkdj-1bnsbsb-bsbsb0011-11sbs",
+    name: "Edward",
+    street: "La Vega",
+    citi: "La Vega",
+    phone: "294829829",
+  },
+  {
+    id: "ahhaha-1bnsbsb-bsbsb0011-11sbs",
+    name: "Edward",
+    street: "La Vega",
+    citi: "La Vega",
+    phone: "294829829",
+  },
+  {
+    id: "ahahahh-aaaaaa-asbsb0011-11sbs",
+    name: "Edward",
+    street: "La Vega",
+    citi: "La Vega",
+    phone: "294829829",
+  },
+];
+
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person]!
+  }
+`;
