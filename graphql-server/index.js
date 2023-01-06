49e1755240c757375b81d2943ea23201d5a70ce0
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2023-01-06 13:38:12 -0400

    compleject resolvers

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -5,44 +5,48 @@ const persons = [
     id: "jhahahh-1bnsbsb-bsbsb0011-11sbs",
     name: "Edward",
     street: "La Vega",
-    citi: "La Vega",
+    city: "La Vega",
     phone: "294829829",
   },
   {
     id: "kdkdj-1bnsbsb-bsbsb0011-11sbs",
     name: "Edward",
     street: "La Vega",
-    citi: "La Vega",
+    city: "La Vega",
     phone: "294829829",
   },
   {
     id: "ahhaha-1bnsbsb-bsbsb0011-11sbs",
     name: "Edward",
     street: "La Vega",
-    citi: "La Vega",
+    city: "La Vega",
     phone: "294829829",
   },
   {
     id: "ahahahh-aaaaaa-asbsb0011-11sbs",
     name: "Edward",
     street: "La Vega",
-    citi: "La Vega",
+    city: "La Vega",
     phone: "294829829",
   },
 ];
 
 const typeDefs = gql`
+  type Adress {
+    street: String!
+    city: String!
+  }
   type Person {
     name: String!
     phone: String
-    street: String!
-    city: String!
+    adress: Adress!
     id: ID!
   }
 
   type Query {
     personCount: Int!
     allPersons: [Person]!
+    findPerson(name: String!): Person
   }
 `;
 
@@ -50,6 +54,18 @@ const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
+    findPerson: (root, args) => {
+      const { name } = args;
+      return persons.find((person) => person.name === name);
+    },
+  },
+  Person: {
+    adress: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
   },
 };
 
