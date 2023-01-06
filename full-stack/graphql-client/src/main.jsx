6b0d85e60f1f146f2c260f5c9aa32af201cef910
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2023-01-06 15:21:19 -0400

    config apollo client

--- full-stack/graphql-client/src/main.jsx
+++ full-stack/graphql-client/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ApolloClient, gql, HttpLink, InMemoryCache } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000/",
+  }),
+});
+
+const query = gql`
+query{
+  allPersons{
+  id
+  name
+  phone
+  adress{
+    city
+    street
+  }
+}
+}
+`
+client.query({query}).then(res =>console.log(res.data))
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
