dbb2c8956cdd76a3d46c7411a22c1b219433031f
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2023-01-12 13:58:08 -0400

    learning MUI

---
+++ full-stack/vite-project/src/components/MUIButton.tsx
@@ -0,0 +1,55 @@
+import { Button, Stack, Typography } from "@mui/material";
+import React from "react";
+import SendIcon from '@mui/icons-material/Send';
+const MUIButton = () => {
+  return (
+    <>
+      <Typography variant="h1">BUTTONS</Typography>
+      <Stack spacing={4}>
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">contained</Button>
+          <Button variant="outlined">outlined</Button>
+        </Stack>
+        <Stack spacing={2} direction="row">
+          <Button variant="contained" color="primary">
+            primary
+          </Button>
+          <Button variant="contained" color="secondary">
+            secondary
+          </Button>
+          <Button variant="contained" color="error">
+            error
+          </Button>
+          <Button variant="contained" color="warning">
+            warning
+          </Button>
+          <Button variant="contained" color="info">
+            info
+          </Button>
+          <Button variant="contained" color="success">
+            success
+          </Button>
+        </Stack>
+        <Stack display='block' spacing={2} direction='row'>
+        <Button variant="contained" size="small">
+        small
+          </Button>
+        <Button variant="contained" size="medium">
+        medium
+          </Button>
+
+        <Button variant="contained" size="large">
+        large
+          </Button>
+        </Stack>
+        <Stack spacing={2} direction='row'>
+          <Button variant="contained" startIcon={<SendIcon/>}>Send</Button>
+          <Button variant="contained" endIcon={<SendIcon/>}>Send</Button>
+        </Stack>
+      </Stack>
+    </>
+  );
+};
+
+export default MUIButton;
